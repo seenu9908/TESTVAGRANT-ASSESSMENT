679f6e7c2d0f518fc5809085679c42579f2114d7
seenu9908 <112007041+seenu9908@users.noreply.github.com>
2022-08-25 12:15:15 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,10 @@
+import unittest
+class TestingSum(unittest.TestCase):
+    def test_moons():
+        self.assertEqual(count_of_moons(collection),189,"It should be 189")
+        
+    def test_surface_gas():
+        self.assertEqual(gas_retrival(collection),Hydrogen,"It should be Hydrogen")
+        
+if _name=='main_':
+    unittest.main()
